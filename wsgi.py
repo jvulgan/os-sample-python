a73b48074e8abb49b19bc9174d7d2ccac1c1c3bd
Jakub Vulgan <jvulgan@redhat.com>
2019-05-31 09:27:56 +0200

    Add /host endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,19 @@
+import os
+
 from flask import Flask
+
 application = Flask(__name__)
+
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+
+@application.route("/host")
+def replicas():
+    return "Host: {}".format(os.getenv("HOSTNAME", "unknown"))
+
 
 if __name__ == "__main__":
     application.run()
